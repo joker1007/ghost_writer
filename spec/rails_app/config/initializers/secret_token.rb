07e1b318102136d145870193e43b9b51dd5259b9
joker1007 <kakyoin.hierophant@gmail.com>
2013-08-06 20:43:39 +0900

    Fix deprecation warning

--- spec/rails_app/config/initializers/secret_token.rb
+++ spec/rails_app/config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-RailsApp::Application.config.secret_token = '98e50fa60d0ddcf9c64b8b87e0dfd8c247885c4cbcaeecf37d24fc4b5fb2cd36c9744a5165653efc77d5ced8cb4a6c7157d3752721b0acc2e7b69b7f2b915b9a'
+RailsApp::Application.config.secret_key_base = '98e50fa60d0ddcf9c64b8b87e0dfd8c247885c4cbcaeecf37d24fc4b5fb2cd36c9744a5165653efc77d5ced8cb4a6c7157d3752721b0acc2e7b69b7f2b915b9a'
